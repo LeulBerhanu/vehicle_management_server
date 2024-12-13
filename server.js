2acd096aaa408579859303ba4bbd61f1220998f8
LeulBerhanu <leulberhanua@gmail.com>
2024-12-13 16:46:05 +0300

    add vehicles route, express.json, error & not found handler middleware

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 require("dotenv").config();
 const express = require("express");
 const connectDB = require("./src/utiles/connectDB");
+const vehicleRouter = require("./src/routes/vehicle.routes");
 
 const app = express();
 
 connectDB();
+
+app.use(express.json());
+
+app.use("/api/vehicles", vehicleRouter);
+
+// Not found handler
+app.use((req, res, next) => {
+  res.status(404).json({ message: "Route not found on our server" });
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  const message = err.message;
+  const stack = err.stack;
+  res.status(500).send({ message, stack });
+});
 
 app.listen(
   process.env.PORT,
