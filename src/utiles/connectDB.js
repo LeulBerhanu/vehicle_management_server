82380a7a1b0e4fba07d6f4b64282d3c723dbf3ee
LeulBerhanu <leulberhanua@gmail.com>
2024-12-16 19:40:49 +0300

    add console log

--- src/utiles/connectDB.js
+++ src/utiles/connectDB.js
@@ -2,8 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("Connecting to database ...");
     const connection = await mongoose.connect(process.env.MONGO_URL);
     console.log(`MongoDB connected: ${connection.connection.host}`);
+    return connection;
   } catch (error) {
     console.log(`Error connecting to Mongodb`, error);
     process.exit(1);
