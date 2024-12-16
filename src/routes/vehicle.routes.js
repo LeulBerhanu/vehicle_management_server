074f3b8ab126e3b4b9abf9884f57865954b70319
LeulBerhanu <leulberhanua@gmail.com>
2024-12-16 19:39:26 +0300

    add delete many route

--- src/routes/vehicle.routes.js
+++ src/routes/vehicle.routes.js
@@ -8,5 +8,6 @@ vehicleRouter.get("/:id", vehicleController.detail);
 vehicleRouter.post("/", vehicleController.create);
 vehicleRouter.put("/:id", vehicleController.update);
 vehicleRouter.delete("/:id", vehicleController.delete);
+vehicleRouter.post("/delete-many", vehicleController.deleteMany);
 
 module.exports = vehicleRouter;
