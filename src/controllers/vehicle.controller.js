667bed12f8caf1d217c25b377c008c56dd656e80
LeulBerhanu <leulberhanua@gmail.com>
2024-12-16 19:38:54 +0300

    add pagination

--- src/controllers/vehicle.controller.js
+++ src/controllers/vehicle.controller.js
@@ -1,14 +1,52 @@
 const asyncHandler = require("express-async-handler");
 const Vehicle = require("../models/Vehicle.model");
-const { validateVehicle } = require("../validators/vehicle.validator");
+const {
+  validateVehicle,
+  statuses,
+} = require("../validators/vehicle.validator");
 const { isValidObjectId } = require("mongoose");
 
 const vehicleController = {
   list: asyncHandler(async (req, res) => {
-    const vehicles = await Vehicle.find();
-    res
-      .status(200)
-      .json({ message: "Vehicles fetched successfully.", vehicles });
+    const { page = 1, limit = 10, filter, sort } = req.query;
+
+    const query = {};
+    if (filter) {
+      query.$or = [
+        { name: { $regex: filter, $options: "i" } },
+        { status: { $regex: filter, $options: "i" } },
+      ];
+    }
+
+    const currentPage = Math.max(1, parseInt(page));
+    const pageSize = Math.max(1, parseInt(limit));
+
+    const sortObj = {};
+    if (sort && sort.length > 0) {
+      sort.forEach(({ id: sortField, desc }) => {
+        if (sortField) {
+          sortObj[sortField] = JSON.parse(desc) ? -1 : 1;
+        }
+      });
+    }
+
+    const vehicles = await Vehicle.find(query)
+      .sort(sortObj)
+      .skip(page * limit)
+      .limit(limit);
+
+    const totalItems = await Vehicle.countDocuments(query);
+
+    res.status(200).json({
+      message: "Vehicles fetched successfully.",
+      vehicles,
+      pagination: {
+        currentPage,
+        pageSize,
+        limit,
+        totalItems,
+      },
+    });
   }),
 
   detail: asyncHandler(async (req, res) => {
@@ -41,14 +79,12 @@ const vehicleController = {
       return res.status(400).json({ message: "Validation failed!", error });
     }
 
-    const vehicleUpdated = await Vehicle.findByIdAndUpdate(vehicleId, data, {
+    const vehicle = await Vehicle.findByIdAndUpdate(vehicleId, data, {
       new: true,
     });
-    if (!vehicleUpdated) throw new Error("Vehicle not found");
+    if (!vehicle) throw new Error("Vehicle not found");
 
-    res
-      .status(200)
-      .json({ message: "Vehicle updated successfully", vehicleUpdated });
+    res.status(200).json({ message: "Vehicle updated successfully", vehicle });
   }),
 
   delete: asyncHandler(async (req, res) => {
@@ -59,6 +95,22 @@ const vehicleController = {
     if (!vehicleFound) throw new Error("Vehicle not found");
 
     res.status(200).json({ message: "Vehicle deleted successfully" });
+  }),
+
+  deleteMany: asyncHandler(async (req, res) => {
+    const vehicleIds = req.body;
+
+    vehicleIds.forEach((id) => {
+      if (!isValidObjectId(id)) throw new Error("Id not valid");
+    });
+
+    const deletedVehicles = await Vehicle.deleteMany({
+      _id: { $in: vehicleIds },
+    });
+
+    res.status(200).json({
+      message: `${deletedVehicles.deletedCount} vehicles deleted successfully`,
+    });
   }),
 };
 
